03a628831ee78e3784dcc785d91b437d83f46993
ssh00n <126448602+ssh00n@users.noreply.github.com>
2023-03-25 10:33:11 +0900

    Add files via upload

---
+++ ssh00n/11047_동전_0.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+coins = [int(input()) for _ in range(n)]
+
+cnt = 0
+for i in range(len(coins)-1, -1, -1):
+    if k == 0:
+        break
+    cnt += k // coins[i]
+    k = k % coins[i]
+    
+    
+print(cnt)
